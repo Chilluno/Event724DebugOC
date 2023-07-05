f0fbe5df52c97f75d4ac31fc3c87788686f34f33
Wesley Slimani <wesley.slimani@gmail.com>
2023-07-05 10:07:45 +0200

    fix: changed the false condition for type to have the events filtered by type

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,7 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      : data?.events.filter((event) => event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
@@ -42,7 +42,7 @@ const EventList = () => {
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={(value) =>  (value ? changeType(value) : changeType(value)) }
           />
           <div id="events" className="ListContainer">
             {filteredEvents.map((event) => (
